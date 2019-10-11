ab4702365fac7c4c35f322016255de99dfd221b2
Dattu Chillal <chillalkar@gmail.com>
2019-10-11 16:13:11 +0530

    Update GatewayApplication.java

--- src/main/java/com/cg/gateway/GatewayApplication.java
+++ src/main/java/com/cg/gateway/GatewayApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.ribbon.RibbonClients;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 @EnableEurekaClient
 @EnableZuulProxy
